f77d4efb958385f205d4e0ebd5d206a4ce696d26
HuynhNgocThuong <huynhngocthuong0208@gmail.com>
2024-10-16 12:25:27 +0700

    [Modify] Bottom table

--- src/components/ui/table/data-table.tsx
+++ src/components/ui/table/data-table.tsx
@@ -55,13 +55,28 @@ export function DataTable<TData, TValue>({
   );
 
   const paginationState = {
-    pageIndex: currentPage - 1, // zero-based index for React Table
+    pageIndex: currentPage - 1,
     pageSize: pageSize
   };
 
   const pageCount = Math.ceil(totalItems / pageSize);
 
-  const handlePaginationChange = () => {};
+  const handlePaginationChange = (
+    updaterOrValue:
+      | PaginationState
+      | ((old: PaginationState) => PaginationState)
+  ) => {
+    console.log('🚀 ~ updaterOrValue:', updaterOrValue);
+    console.log('🚀 ~ paginationState:', paginationState);
+    const pagination =
+      typeof updaterOrValue === 'function'
+        ? updaterOrValue(paginationState)
+        : updaterOrValue;
+    console.log('🚀 ~ pagination:', pagination);
+
+    setCurrentPage(pagination.pageIndex + 1); // converting zero-based index to one-based
+    setPageSize(pagination.pageSize);
+  };
 
   const table = useReactTable({
     data,
@@ -129,7 +144,99 @@ export function DataTable<TData, TValue>({
         </Table>
         <ScrollBar orientation="horizontal" />
       </ScrollArea>
-      <div></div>
+      <div className="flex flex-col items-center justify-end gap-2 space-x-2 py-4 sm:flex-row">
+        <div className="flex w-full items-center justify-between">
+          <div className="flex-1 text-sm text-muted-foreground">
+            {totalItems > 0 ? (
+              <>
+                Showing{' '}
+                {paginationState.pageIndex * paginationState.pageSize + 1} to{' '}
+                {Math.min(
+                  (paginationState.pageIndex + 1) * paginationState.pageSize,
+                  totalItems
+                )}{' '}
+                of {totalItems} entries
+              </>
+            ) : (
+              'No entries found'
+            )}
+          </div>
+          <div className="flex flex-col items-center gap-4 sm:flex-row sm:gap-6 lg:gap-8">
+            <div className="flex items-center space-x-2">
+              <p className="whitespace-nowrap text-sm font-medium">
+                Rows per page
+              </p>
+              <Select
+                value={`${paginationState.pageSize}`}
+                onValueChange={(value) => {
+                  table.setPageSize(Number(value));
+                }}
+              >
+                <SelectTrigger className="h-8 w-[70px]">
+                  <SelectValue placeholder={paginationState.pageSize} />
+                </SelectTrigger>
+                <SelectContent side="top">
+                  {pageSizeOptions.map((pageSize) => (
+                    <SelectItem key={pageSize} value={`${pageSize}`}>
+                      {pageSize}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-full items-center justify-between gap-2 sm:justify-end">
+          <div className="flex w-[150px] items-center justify-center text-sm font-medium">
+            {totalItems > 0 ? (
+              <>
+                Page {paginationState.pageIndex + 1} of {table.getPageCount()}
+              </>
+            ) : (
+              'No pages'
+            )}
+          </div>
+          <div className="flex items-center space-x-2">
+            <Button
+              aria-label="Go to first page"
+              variant="outline"
+              className="h-8 w-8 p-0"
+              onClick={() => table.setPageIndex(0)}
+              disabled={!table.getCanPreviousPage()}
+            >
+              <DoubleArrowLeftIcon className="h-4 w-4" aria-hidden="true" />
+            </Button>
+            <Button
+              aria-label="Go to previous page"
+              variant="outline"
+              className="h-8 w-8 p-0"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              {' '}
+              <ChevronLeftIcon className="h-4 w-4" aria-hidden="true" />
+            </Button>
+            <Button
+              aria-label="Go to next page"
+              variant="outline"
+              className="h-8 w-8 p-0"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              <ChevronRightIcon className="h-4 w-4" aria-hidden="true" />
+            </Button>
+            <Button
+              aria-label="Go to last page"
+              variant="outline"
+              className="hidden h-8 w-8 p-0 lg:flex"
+              onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+              disabled={!table.getCanNextPage()}
+            >
+              <DoubleArrowRightIcon className="h-4 w-4" aria-hidden="true" />
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/sections/employee/employee-tables/index.tsx
+++ src/sections/employee/employee-tables/index.tsx
@@ -3,6 +3,7 @@
 import { DataTable } from '@/components/ui/table/data-table';
 import { Employee } from '@/constants/data';
 import { columns } from './colums';
+import { useEmployeeTableFilters } from './use-employee-table-filters';
 
 export default function EmployeeTable({
   data,
@@ -11,6 +12,16 @@ export default function EmployeeTable({
   data: Employee[];
   totalData: number;
 }) {
+  const {
+    genderFilter,
+    setGenderFilter,
+    isAnyFilterActive,
+    resetFilters,
+    searchQuery,
+    setPage,
+    setSearchQuery
+  } = useEmployeeTableFilters();
+
   return (
     <div className="space-y-4">
       <div className="flex flex-wrap items-center gap-4">hehe</div>
