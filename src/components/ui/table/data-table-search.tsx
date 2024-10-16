05d2d88b7973a44d7b4ebd4db320fc0af29cd00c
HuynhNgocThuong <huynhngocthuong0208@gmail.com>
2024-10-16 23:12:21 +0700

    [Mdoify] Add file

---
+++ src/components/ui/table/data-table-search.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { Input } from '@/components/ui/input';
+import { Options } from 'nuqs';
+import { useTransition } from 'react';
+
+interface DataTableSearchProps {
+  searchKey: string;
+  searchQuery: string;
+  setSearchQuery: (
+    value: string | ((old: string) => string | null) | null,
+    options?: Options<any> | undefined
+  ) => Promise<URLSearchParams>;
+  setPage: <Shallow>(
+    value: number | ((old: number) => number | null) | null,
+    options?: Options<Shallow> | undefined
+  ) => Promise<URLSearchParams>;
+}
+
+export function DataTableSearch({
+  searchKey,
+  searchQuery,
+  setSearchQuery,
+  setPage
+}: DataTableSearchProps) {
+  const [isLoading, startTransition] = useTransition();
+
+  const handleSearch = (value: string) => {
+    setSearchQuery(value, { startTransition });
+    setPage(1);
+  };
+  return (
+    <Input
+      placeholder={`Search ${searchKey}...`}
+      value={searchQuery ?? ''}
+      onChange={(e) => handleSearch(e.target.value)}
+      className=""
+    />
+  );
+}
--- src/sections/employee/employee-tables/index.tsx
+++ src/sections/employee/employee-tables/index.tsx
@@ -4,6 +4,7 @@ import { DataTable } from '@/components/ui/table/data-table';
 import { Employee } from '@/constants/data';
 import { columns } from './colums';
 import { useEmployeeTableFilters } from './use-employee-table-filters';
+import { DataTableSearch } from '@/components/ui/table/data-table-search';
 
 export default function EmployeeTable({
   data,
@@ -24,7 +25,14 @@ export default function EmployeeTable({
 
   return (
     <div className="space-y-4">
-      <div className="flex flex-wrap items-center gap-4">hehe</div>
+      <div className="flex flex-wrap items-center gap-4">
+        <DataTableSearch
+          searchKey="name"
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+          setPage={setPage}
+        />
+      </div>
       <DataTable columns={columns} data={data} totalItems={totalData} />
     </div>
   );
